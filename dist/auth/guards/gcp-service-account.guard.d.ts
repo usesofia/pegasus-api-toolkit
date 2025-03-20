c02cb68a3fd6aa33896e44ba108aaa6e0277548a
Marco Aurélio Prado <marco.pdsv@gmail.com>
2025-03-20 15:19:44 -0300

    refactor(auth): remove MockGcpServiceAccountGuard and refactor test handling

--- dist/auth/guards/gcp-service-account.guard.d.ts
+++ dist/auth/guards/gcp-service-account.guard.d.ts
@@ -4,11 +4,6 @@ import { BaseConfigEntity } from '../../config/base-config.entity';
 import { ClsService } from 'nestjs-cls';
 import { Base } from '../../base';
 export declare const GCP_SERVICE_ACCOUNT_TOKEN_FOR_TESTS = "gcp-service-account-token";
-export declare class MockGcpServiceAccountGuard implements CanActivate {
-    private reflector;
-    constructor(reflector: Reflector);
-    canActivate(context: ExecutionContext): boolean;
-}
 export declare class GcpServiceAccountGuard extends Base implements CanActivate {
     protected readonly baseConfig: BaseConfigEntity;
     protected readonly logger: LoggerService;
