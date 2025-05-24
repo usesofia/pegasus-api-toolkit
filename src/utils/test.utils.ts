83a41551d8e9afb82555d53f5e8142e2ff2184ec
Marco Aurélio Prado <marco.pdsv@gmail.com>
2025-05-24 16:59:38 -0300

    refactor: remove unused SecondaryMongoDbDatabaseModule from test utilities - Clean up test.utils.ts by removing SecondaryMongoDbDatabaseModule import and usage - Simplify test setup configuration - Maintain existing functionality with cleaner code structure

--- src/utils/test.utils.ts
+++ src/utils/test.utils.ts
@@ -28,7 +28,6 @@ import { TasksModule } from '@app/tasks/tasks.module';
 import { BASE_CONFIG, BaseConfigEntity } from '@app/config/base-config.entity';
 import { SentryModule } from '@app/sentry/sentry.module';
 import { FilesModule } from '@app/files/files.module';
-import { SecondaryMongoDbDatabaseModule } from '@app/database/secondary-mongodb-database.module';
 
 EventEmitter.defaultMaxListeners = 128;
 
@@ -91,7 +90,6 @@ export class InstanceFixture {
         LoggerModule,
         buildClsModule(),
         PrimaryMongoDbDatabaseModule,
-        SecondaryMongoDbDatabaseModule,
         PubSubModule,
         TasksModule,
         CacheModule,
