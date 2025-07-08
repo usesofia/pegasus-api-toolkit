a1d68c8a78498d44f72993089b472ad8e67a131f
Marco Aurélio Prado <marco.pdsv@gmail.com>
2025-07-08 17:08:23 -0300

    ...

--- dist/src/utils/entity.utils.js
+++ dist/src/utils/entity.utils.js
@@ -3,33 +3,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.safeInstantiateEntity = safeInstantiateEntity;
 exports.unsafeInstantiateEntity = unsafeInstantiateEntity;
 const json_utils_1 = require("./json.utils");
-const common_1 = require("@nestjs/common");
 function createInstance(c) {
     return new c();
 }
 function safeInstantiateEntity(entityClass, input) {
-    try {
-        const entityInstance = createInstance(entityClass);
-        const safeInput = JSON.parse(JSON.stringify(input, (0, json_utils_1.getJsonStringifyReplacer)()), (0, json_utils_1.getJsonParseReviver)());
-        const validProps = entityClass.create(safeInput);
-        Object.assign(entityInstance, validProps);
-        Object.freeze(entityInstance);
-        return entityInstance;
-    }
-    catch (error) {
-        throw new common_1.InternalServerErrorException(error);
-    }
+    const entityInstance = createInstance(entityClass);
+    const safeInput = JSON.parse(JSON.stringify(input, (0, json_utils_1.getJsonStringifyReplacer)()), (0, json_utils_1.getJsonParseReviver)());
+    const validProps = entityClass.create(safeInput);
+    Object.assign(entityInstance, validProps);
+    Object.freeze(entityInstance);
+    return entityInstance;
 }
 function unsafeInstantiateEntity(entityClass, input) {
     const entityInstance = createInstance(entityClass);
-    try {
-        const validProps = entityClass.create(input);
-        Object.assign(entityInstance, validProps);
-        Object.freeze(entityInstance);
-        return entityInstance;
-    }
-    catch (error) {
-        throw new common_1.InternalServerErrorException(error);
-    }
+    const validProps = entityClass.create(input);
+    Object.assign(entityInstance, validProps);
+    Object.freeze(entityInstance);
+    return entityInstance;
 }
 //# sourceMappingURL=entity.utils.js.map
--- src/utils/entity.utils.ts
+++ src/utils/entity.utils.ts
@@ -2,7 +2,6 @@ import {
   getJsonParseReviver,
   getJsonStringifyReplacer,
 } from '@app/utils/json.utils';
-import { InternalServerErrorException } from '@nestjs/common';
 import { ZodDto } from 'nestjs-zod';
 
 function createInstance<T extends ZodDto>(c: new () => T): T {
@@ -14,19 +13,15 @@ export function safeInstantiateEntity<T extends ZodDto>(
   entityClass: T,
   input: any,
 ): InstanceType<T> {
-  try {
-    const entityInstance = createInstance(entityClass);
-    const safeInput = JSON.parse(
-      JSON.stringify(input, getJsonStringifyReplacer()),
-      getJsonParseReviver(),
-    );
-    const validProps = entityClass.create(safeInput);
-    Object.assign(entityInstance, validProps);
-    Object.freeze(entityInstance);
-    return entityInstance;
-  } catch (error) {
-    throw new InternalServerErrorException(error);
-  }
+  const entityInstance = createInstance(entityClass);
+  const safeInput = JSON.parse(
+    JSON.stringify(input, getJsonStringifyReplacer()),
+    getJsonParseReviver(),
+  );
+  const validProps = entityClass.create(safeInput);
+  Object.assign(entityInstance, validProps);
+  Object.freeze(entityInstance);
+  return entityInstance;
 }
 
 export function unsafeInstantiateEntity<T extends ZodDto>(
@@ -34,13 +29,9 @@ export function unsafeInstantiateEntity<T extends ZodDto>(
   input: any,
 ): InstanceType<T> {
   const entityInstance = createInstance(entityClass);
-  try {
-    const validProps = entityClass.create(input);
-    Object.assign(entityInstance, validProps);
-    Object.freeze(entityInstance);
-    return entityInstance;
-  } catch (error) {
-    throw new InternalServerErrorException(error);
-  }
+  const validProps = entityClass.create(input);
+  Object.assign(entityInstance, validProps);
+  Object.freeze(entityInstance);
+  return entityInstance;
 }
 /* eslint-enable */
